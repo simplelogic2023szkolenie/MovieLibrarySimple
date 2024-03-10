b6320268eac55c001da03f62de0b12e9470b8af1
Mateusz Tadla <mat@scalepoint.com>
2024-03-10 14:18:57 +0100

    update

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,6 +1,15 @@
 package models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public abstract class Person {
+    // @JsonProperty("nazwaKluczaWJson")
+    // ta adnotacja jest przydatna kiedy w kluczu JSON mamy znaki
+    // których nie możemy użyć jako nazwa pola klasy w javie
+    // czyli np "-". wtedy używamy adnotacji nad polem klasy i bilbioteka
+    // json jackson bedzie deserializować JSONa nie biorąc pod uwagę nazwy pola
+    // ale customową nazwę podaną w @JsonProperty
+    @JsonProperty("first-name")
     private String firstName;
     private String lastName;
 
