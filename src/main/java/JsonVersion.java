35f34c1899910b3552d255aa1f3c6edfd9920e75
Mateusz Tadla <mat@scalepoint.com>
2024-03-10 14:12:33 +0100

    update

--- src/main/java/JsonVersion.java
+++ src/main/java/JsonVersion.java
@@ -1,13 +1,19 @@
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import models.MovieLibrary;
+
+import java.io.File;
+import java.io.IOException;
 
 public class JsonVersion {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
 
+        MovieLibrary movieLibrary = objectMapper.readValue(new File("src/main/resources/movies.json"), MovieLibrary.class);
 
+        System.out.println(movieLibrary);
     }
 }
